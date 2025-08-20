01a0e8cc0addbee799c069567d50204a8113bea8
BONIYA <75773751+BONIYA97@users.noreply.github.com>
2025-08-20 12:52:46 +0530

    Delete backend directory

--- backend/TourPackage/tourapp_back/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Country, City, Package, Schedule, Enquiry,  Banner
-
-admin.site.register(Country)
-admin.site.register(City)
-admin.site.register(Package)
-admin.site.register(Schedule)
-admin.site.register(Enquiry)
-admin.site.register(Banner)
--- backend/TourPackage/tourapp_back/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TourappBackConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tourapp_back'
